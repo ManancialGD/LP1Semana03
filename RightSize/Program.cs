f632f59dad430d1bd97141a35aa56eca27ee35df
“[Breno <manancialgd@gmail.com>
2024-03-08 14:09:44 +0000

    Added the project RightSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (string argumento in args)
+        {
+            if (argumento.Length > 6)
+            {
+                return;
+            }
+
+            if (argumento.Length > 3)
+            {
+                Console.WriteLine(argumento);
+            }
+        }
+    }
+}
