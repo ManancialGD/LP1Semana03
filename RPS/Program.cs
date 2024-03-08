f6b79a443711078abd24c44ec13de619a4834632
“[Breno <manancialgd@gmail.com>
2024-03-08 15:15:47 +0000

    Added RPS

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -2,11 +2,46 @@
 
 namespace RPS
 {
-    class Program
+        class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GameItem player1Choice = ParseGameItem(args[0]);
+            GameItem player2Choice = ParseGameItem(args[1]);
+
+            GameStatus result = RockPaperScissors(player1Choice, player2Choice);
+
+            switch (result)
+            {
+                case GameStatus.Draw:
+                    Console.WriteLine("It's a draw!");
+                    break;
+                case GameStatus.Player1Wins:
+                    Console.WriteLine("Player 1 wins!");
+                    break;
+                case GameStatus.Player2Wins:
+                    Console.WriteLine("Player 2 wins!");
+                    break;
+            }
+        }
+
+        private static GameItem ParseGameItem(string input)
+        {
+            return Enum.Parse<GameItem>(input);
+        }
+
+        private static GameStatus RockPaperScissors(GameItem player1, GameItem player2)
+        {
+            if (player1 == player2)
+            {
+                return GameStatus.Draw;
+            }
+
+            bool player1Wins = (player1 == GameItem.Rock && player2 == GameItem.Scissors) ||
+                            (player1 == GameItem.Scissors && player2 == GameItem.Paper) ||
+                            (player1 == GameItem.Paper && player2 == GameItem.Rock);
+
+            return player1Wins ? GameStatus.Player1Wins : GameStatus.Player2Wins;
         }
     }
 }
