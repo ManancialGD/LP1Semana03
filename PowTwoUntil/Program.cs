ce9ca0c445ff45f0d187a9dca661847de42a2d4a
“[Breno <manancialgd@gmail.com>
2024-03-08 15:08:59 +0000

    Added the project PowTwoUntil

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowersOf2Until5();
+            PowersOf2Until5();
+        }
+
+        private static void PowersOf2Until5()
+        {
+        for (int i = 1; i <= (1 << 5); i = i << 1)
+        {
+            Console.WriteLine(i);
+        }
+        }
+
+    }
+    
+}
